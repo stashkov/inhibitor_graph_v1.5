921b7d2cb4d0a253a4fbd88a062c4013ea9b2a6e
stashkov <testct2@gmail.com>
2017-08-09 19:48:57 +0200

    add weights and support for inhibited edges

--- tests_expand.py
+++ tests_expand.py
@@ -5,25 +5,54 @@ import networkx as nx
 class ExpandGraph(object):
     def __init__(self, graph):
         assert isinstance(graph, nx.DiGraph)
+        assert all('weight' in d.keys() for u, v, d in graph.edges(data=True)), \
+            "all edges must have weights"
         self.graph, self.additional_nodes = ExpandGraph.expand_graph(graph)
         self.matrix = list()
         self.vector = list()
         self.initialize_matrix_and_vector()
         self.fill_in_stoichiometric_matrix()
 
+    @property
+    def reactant(self):
+        """
+        terms that are on the left in chemical equation
+        in stoichiometric matrices denoted by -1
+        """
+        return -1
+
+    @property
+    def product(self):
+        """
+        terms that are on the right in chemical equation
+        in stoichiometric matrices denoted by 1
+        """
+        return 1
+
     def initialize_matrix_and_vector(self):
+        """
+        As per article,
+            each activation gives us 3 reactions
+            each inhibition gives us 2 reactions
+        """
         rows = len(self.graph.nodes())
-        columns = 3 * len(self.graph.edges())  # TODO each activation edge gives 3 reactions, but inhibition gives 2
+        activation_constant = 3
+        inhibition_constant = 2
+        n_of_activation_reactions = \
+            activation_constant * sum(1 for _, _, d in self.graph.edges(data=True) if d['weight'] == 0)
+        n_of_inhibition_reactions = \
+            inhibition_constant * sum(1 for _, _, d in self.graph.edges(data=True) if d['weight'] == 1)
+        columns = n_of_activation_reactions + n_of_inhibition_reactions
         self.matrix = self.generate_empty_stoichiometric_matrix(rows, columns)
-        self.vector = [0 for _ in range(len(self.graph.edges()))]
+        self.vector = [0 for _ in range(columns)]
 
     def fill_in_stoichiometric_matrix(self):
-        assert all('weight' in d.keys() for u, v, d in self.graph.edges(data=True)), \
-            "all nodes must have weights"
         reaction_number = 0
         for edge in self.graph.edges():
             if self.graph.get_edge_data(*edge)['weight'] == 0:
                 reaction_number = self.add_activation_edge_reactions(edge, reaction_number)
+            if self.graph.get_edge_data(*edge)['weight'] == 1:
+                reaction_number = self.add_inhibition_edge_reactions(edge, reaction_number)
 
     def add_activation_edge_reactions(self, edge, reaction_number):
         """
@@ -45,42 +74,90 @@ class ExpandGraph(object):
 
         and reversibility vector [1, 0, 0]
         """
+        reversible_reaction = 1
         assert isinstance(edge, tuple)
         u, v = edge
-        self.add_first_reaction(u, v, reaction_number)
+        self.add_first_activation_reaction(u, v, reaction_number)
+        self.vector[reaction_number] = reversible_reaction
         reaction_number += 1
-        self.add_second_reaction(u, v, reaction_number)
+        self.add_second_activation_reaction(u, v, reaction_number)
         reaction_number += 1
-        self.add_third_reaction(v, reaction_number)
+        self.add_third_activation_reaction(v, reaction_number)
         return reaction_number
 
-    def add_first_reaction(self, u, v, column):
+    def add_inhibition_edge_reactions(self, edge, reaction_number):
+        """
+        Given edge GRK2 -| GEF
+
+        Re-write it as system of reactions:
+        GRK2 + GEF <-> GRK2:not(GEF)
+        GRK2:not(GEF) -> GRK2 + not(GEF)
+
+        Expands it
+
+        to stoichiometric matrix
+                        reaction1    reaction2
+        GRK2               -1            1
+        GEF                -1            0
+        not(GEF)            0            1
+        GRK2:not(GEF)       1           -1
+
+        and reversibility vector [1, 0, 0]
+        """
+        reversible_reaction = 1
+        assert isinstance(edge, tuple)
+        u, v = edge
+        self.add_first_inhibition_reaction(u, v, reaction_number)
+        self.vector[reaction_number] = reversible_reaction
+        reaction_number += 1
+        self.add_second_inhibition_reaction(u, v, reaction_number)
+        return reaction_number
+
+    def add_first_inhibition_reaction(self, u, v, column):
+        # print("First Reaction: {} + {} <-> {}".format(u, v, self.additional_nodes[(u, v)]))
+        self.matrix[u - 1][column] = self.reactant
+        self.matrix[v - 1][column] = self.reactant
+        self.matrix[self.additional_nodes[(u, v)] - 1][column] = self.product
+
+    def add_second_inhibition_reaction(self, u, v, column):
+        # print("Second Reaction: {} -> {} + {}".format(self.additional_nodes[(u, v)], u, self.additional_nodes[v]))
+        self.matrix[self.additional_nodes[(u, v)] - 1][column] = self.reactant
+        self.matrix[u - 1][column] = self.product
+        self.matrix[self.additional_nodes[v] - 1][column] = self.product
+
+    def add_first_activation_reaction(self, u, v, column):
         # print("First Reaction: {} + {} <-> {}".format(u, self.additional_nodes[v], self.additional_nodes[(u, v)]))
-        self.matrix[u - 1][column] = -1
-        self.matrix[self.additional_nodes[v] - 1][column] = -1
-        self.matrix[self.additional_nodes[(u, v)] - 1][column] = 1
+        self.matrix[u - 1][column] = self.reactant
+        self.matrix[self.additional_nodes[v] - 1][column] = self.reactant
+        self.matrix[self.additional_nodes[(u, v)] - 1][column] = self.product
 
-    def add_second_reaction(self, u, v, column):
+    def add_second_activation_reaction(self, u, v, column):
         # print("Second Reaction: {} -> {} + {}".format(self.additional_nodes[(u, v)], u, v))
-        self.matrix[self.additional_nodes[(u, v)] - 1][column] = -1
-        self.matrix[u - 1][column] = 1
-        self.matrix[v - 1][column] = 1
+        self.matrix[self.additional_nodes[(u, v)] - 1][column] = self.reactant
+        self.matrix[u - 1][column] = self.product
+        self.matrix[v - 1][column] = self.product
 
-    def add_third_reaction(self, v, column):
+    def add_third_activation_reaction(self, v, column):
         # print("Third Reaction: {} -> {}".format(v, self.additional_nodes[v]))
-        self.matrix[v - 1][column] = -1
-        self.matrix[self.additional_nodes[v] - 1][column] = 1
+        self.matrix[v - 1][column] = self.reactant
+        self.matrix[self.additional_nodes[v] - 1][column] = self.product
 
     @staticmethod
     def generate_empty_stoichiometric_matrix(number_of_rows, number_of_columns):
         return [[0 for _ in range(number_of_columns)] for _ in range(number_of_rows)]
 
     @staticmethod
-    def add_composite_nodes(additional_nodes, graph):  # TODO composite node
+    def add_composite_nodes(additional_nodes, graph):
+        assert all('weight' in d.keys() for _, _, d in graph.edges(data=True)), \
+            "all edges must have weights"
         next_node_number = max(graph.nodes())
         for edge in graph.edges():
             next_node_number += 1
-            graph.add_node(next_node_number, label='Negation of ' + str(edge))
+            u, v = edge
+            if graph.get_edge_data(*edge)['weight'] == 0:
+                graph.add_node(next_node_number, label='Negation of {} and {}'.format(u, v))
+            if graph.get_edge_data(*edge)['weight'] == 1:
+                graph.add_node(next_node_number, label='Negation of {} and not {}'.format(u, v))
             additional_nodes[edge] = next_node_number
         return graph, additional_nodes
 
@@ -96,6 +173,8 @@ class ExpandGraph(object):
 
     @staticmethod
     def expand_graph(graph):
+        assert all('weight' in d.keys() for u, v, d in graph.edges(data=True)), \
+            "all edges must have weights"
         additional_nodes = dict()
         graph, additional_nodes = ExpandGraph.add_negation_of_nodes(additional_nodes, graph)
         graph, additional_nodes = ExpandGraph.add_composite_nodes(additional_nodes, graph)
@@ -103,7 +182,6 @@ class ExpandGraph(object):
 
 
 class TestsExpansionStep(unittest.TestCase):
-    # @unittest.skip("not implemented")
     def test_expand_activation_edge(self):
         g = nx.DiGraph()
         g.add_nodes_from([1, 2])
@@ -117,10 +195,30 @@ class TestsExpansionStep(unittest.TestCase):
         ]
         vector_gold = [1, 0, 0]
 
-        matrix_lead = ExpandGraph(g).matrix
-        vector_lead = ExpandGraph(g).vector
-        self.assertEqual(matrix_lead, matrix_gold, "matrices are not the same")
-        # self.assertEqual(vector_lead, vector_gold, "vectors are not the same")
+        result = ExpandGraph(g)
+        matrix_lead = result.matrix
+        vector_lead = result.vector
+        self.assertEqual(matrix_lead, matrix_gold)
+        self.assertEqual(vector_lead, vector_gold)
+
+    def test_expand_inhibition_edge(self):
+        g = nx.DiGraph()
+        g.add_nodes_from([1, 2])
+        g.add_edge(1, 2, weight=1)
+
+        matrix_gold = [
+            [-1, 1],
+            [-1, 0],
+            [0, 1],
+            [1, -1]
+        ]
+        vector_gold = [1, 0]
+
+        result = ExpandGraph(g)
+        matrix_lead = result.matrix
+        vector_lead = result.vector
+        self.assertEqual(matrix_lead, matrix_gold)
+        self.assertEqual(vector_lead, vector_gold)
 
 
 class TestStoichiometricMatrix(unittest.TestCase):
@@ -167,9 +265,9 @@ class TestsAddNegationOfNodes(unittest.TestCase):
         additional_nodes_lead = dict()
         graph_lead, additional_nodes_lead = \
             ExpandGraph.add_negation_of_nodes(additional_nodes=additional_nodes_lead, graph=g)
-        self.assertEqual(graph_lead.edges(), g_gold.edges(), "edges are not the same")
-        self.assertEqual(graph_lead.nodes(), g_gold.nodes(), "nodes are not the same")
-        self.assertEqual(additional_nodes_lead, additional_nodes_gold, "dicts of additional nodes are not the same")
+        self.assertEqual(graph_lead.edges(), g_gold.edges())
+        self.assertEqual(graph_lead.nodes(), g_gold.nodes())
+        self.assertEqual(additional_nodes_lead, additional_nodes_gold)
 
 
 class TestAddCompositeNodes(unittest.TestCase):
@@ -183,11 +281,11 @@ class TestAddCompositeNodes(unittest.TestCase):
         """
         g = nx.DiGraph()
         g.add_nodes_from([1, 2])
-        g.add_edge(1, 2)
+        g.add_edge(1, 2, weight=0)
 
         g_gold = nx.DiGraph()
         g_gold.add_nodes_from([1, 2, 3])
-        g_gold.add_edge(1, 2)
+        g_gold.add_edge(1, 2, weight=0)
         additional_nodes_gold = {(1, 2): 3}
 
         additional_nodes_lead = dict()
