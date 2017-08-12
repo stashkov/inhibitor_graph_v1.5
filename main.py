3555b7eac24109a404e3e95cb6bcdda4c14db0e8
stashkov <testct2@gmail.com>
2017-08-12 21:49:05 +0200

    check results

--- main.py
+++ main.py
@@ -5,10 +5,16 @@ result = ExpandGraph(reaction_graph())
 print result.vector
 print result.reactions
 
-for r in [r for v, r in zip(result.vector, result.reactions) if v == 1]:
-    print r
+# for r in [r for v, r in zip(result.vector, result.reactions) if v == 1]:
+#     print r
+print len(result.matrix), 'x', len(result.matrix[0])
 
 m = zip(*result.matrix)
 for i, row in enumerate(m):
     if all(element == 0 for element in row):
         print i, row
+print
+for i, r in enumerate(result.reactions, start=1):
+    print i, r
+
+
