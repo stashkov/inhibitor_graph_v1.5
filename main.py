f7020ba731ad55e4a3ac128fce44abb4c7ff2da2
stashkov <testct2@gmail.com>
2017-08-13 15:08:28 +0200

    list EFMs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from examples.engelhardt_et_al import reaction_graph
 from stoic.generate_stoic import ExpandGraph
+from sampler.sampler import Sampler
 
 result = ExpandGraph(reaction_graph())
 print result.vector
@@ -9,12 +10,22 @@ print result.reactions
 #     print r
 print len(result.matrix), 'x', len(result.matrix[0])
 
-m = zip(*result.matrix)
-for i, row in enumerate(m):
-    if all(element == 0 for element in row):
-        print i, row
-print
-for i, r in enumerate(result.reactions, start=1):
-    print i, r
+# look for columns in stoichiometric matrix that have all 0s
+# m = zip(*result.matrix)
+# for i, row in enumerate(m):
+#     if all(element == 0 for element in row):
+#         print i, row
 
+# print('List all reactions')
+# for i, r in enumerate(result.reactions, start=1):
+#     print i, r
+
+r = Sampler(result.matrix, result.vector)
+print "\nTotal number of EFMs: %s" % len(r.result)
+for i, efms in enumerate(r.result):
+    print 'EFM #%s' % str(i + 1)
+    for index, reaction in enumerate(efms):
+        if reaction == 1:
+            print result.reactions[index]
+    print
 
