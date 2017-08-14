f3c46edd8919ca8ae4cb76415c1b35c1c7f22de3
stashkov <testct2@gmail.com>
2017-08-14 18:05:07 +0200

    rename node info to node name

--- stoic/generate_stoic.py
+++ stoic/generate_stoic.py
@@ -14,7 +14,7 @@ class ExpandGraph(object):
         self.graph = graph
         self.graph, self.additional_nodes = self.expand_graph(graph)
         self.backward_reactions = list()  # track reactions
-        self.matrix , self.vector = self.initialize_matrix_and_vector()
+        self.matrix, self.vector = self.initialize_matrix_and_vector()
         self.reactions = list()
         self.reactants = list()
 
@@ -107,19 +107,19 @@ class ExpandGraph(object):
 
     def add_first_inhibition_reaction(self, u, v, column):
         name = "First Reaction: {} + {} <-> {}".format(
-            self.node_info(u),
-            self.node_info(v),
-            self.node_info(self.additional_nodes[(u, v)]))
+            self.node_name(u),
+            self.node_name(v),
+            self.node_name(self.additional_nodes[(u, v)]))
         self.reactions.append(name)
         self.matrix[u - 1][column] = self.REACTANT
         self.matrix[v - 1][column] = self.REACTANT
         self.matrix[self.additional_nodes[(u, v)] - 1][column] = self.PRODUCT
 
     def add_second_inhibition_reaction(self, u, v, column):
-        name = "Second Reaction: {} -> {} + {}".format(self.node_info(
-            self.additional_nodes[(u, v)]),
-            self.node_info(u),
-            self.node_info(self.additional_nodes[v]))
+        name = "Second Reaction: {} -> {} + {}".format(
+            self.node_name(self.additional_nodes[(u, v)]),
+            self.node_name(u),
+            self.node_name(self.additional_nodes[v]))
         self.reactions.append(name)
         self.matrix[self.additional_nodes[(u, v)] - 1][column] = self.REACTANT
         self.matrix[u - 1][column] = self.PRODUCT
@@ -127,9 +127,9 @@ class ExpandGraph(object):
 
     def add_first_activation_reaction(self, u, v, column):
         name = "First Reaction: {} + {} <-> {}".format(
-            self.node_info(u),
-            self.node_info(self.additional_nodes[v]),
-            self.node_info(self.additional_nodes[(u, v)]))
+            self.node_name(u),
+            self.node_name(self.additional_nodes[v]),
+            self.node_name(self.additional_nodes[(u, v)]))
         self.reactions.append(name)
 
         self.matrix[u - 1][column] = self.REACTANT
@@ -138,9 +138,9 @@ class ExpandGraph(object):
 
     def add_second_activation_reaction(self, u, v, column):
         name = "Second Reaction: {} -> {} + {}".format(
-            self.node_info(self.additional_nodes[(u, v)]),
-            self.node_info(u),
-            self.node_info(v))
+            self.node_name(self.additional_nodes[(u, v)]),
+            self.node_name(u),
+            self.node_name(v))
         self.reactions.append(name)
         self.matrix[self.additional_nodes[(u, v)] - 1][column] = self.REACTANT
         self.matrix[u - 1][column] = self.PRODUCT
@@ -148,20 +148,20 @@ class ExpandGraph(object):
 
     def add_third_activation_reaction(self, v, column):
         name = "Third Reaction: {} -> {}".format(
-            self.node_info(v),
-            self.node_info(self.additional_nodes[v]))
+            self.node_name(v),
+            self.node_name(self.additional_nodes[v]))
         self.reactions.append(name)
         self.matrix[v - 1][column] = self.REACTANT
         self.matrix[self.additional_nodes[v] - 1][column] = self.PRODUCT
         self.backward_reactions.append(v)
 
-    def node_info(self, v):
-        # assert all('info' in d.keys() for _, d in self.graph.nodes(data=True)), \
-        #     "all nodes must have info"
-        if 'info' in self.graph.nodes(v)[0][1]:
-            return nx.get_node_attributes(self.graph, 'info')[v]
+    def node_name(self, v):
+        # assert all('name' in d.keys() for _, d in self.graph.nodes(data=True)), \
+        #     "all nodes must have name"
+        if 'name' in self.graph.nodes(v)[0][1]:
+            return nx.get_node_attributes(self.graph, 'name')[v]
         else:
-            return "node %s has no info" % v
+            return "node %s has no name" % v
 
     @staticmethod
     def generate_empty_stoichiometric_matrix(number_of_rows, number_of_columns):
@@ -174,10 +174,10 @@ class ExpandGraph(object):
         for edge in graph.edges():
             next_node_number += 1
             u, v = edge
-            if graph.get_edge_data(*edge)['weight'] == 0:
-                graph.add_node(next_node_number, info='{} : {}'.format(self.node_info(u), self.node_info(v)))
-            if graph.get_edge_data(*edge)['weight'] == 1:
-                graph.add_node(next_node_number, info='{} : not {}'.format(self.node_info(u), self.node_info(v)))
+            if graph.get_edge_data(*edge)['weight'] == self.ACTIVATION:
+                graph.add_node(next_node_number, name='{} : {}'.format(self.node_name(u), self.node_name(v)))
+            if graph.get_edge_data(*edge)['weight'] == self.INHIBITION:
+                graph.add_node(next_node_number, name='{} : not {}'.format(self.node_name(u), self.node_name(v)))
             additional_nodes[edge] = next_node_number
         return graph, additional_nodes
 
@@ -186,7 +186,7 @@ class ExpandGraph(object):
         for node in graph.nodes():
             if graph.in_degree(node) > 0:
                 next_node_number += 1
-                graph.add_node(next_node_number, info='not ' + self.node_info(node))
+                graph.add_node(next_node_number, name='not ' + self.node_name(node))
                 additional_nodes[node] = next_node_number
         return graph, additional_nodes
 
