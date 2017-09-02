cca6dbd6a7ba17576b03f0c488c38623d89e9cdf
stashkov <testct2@gmail.com>
2017-09-02 21:36:09 +0200

    refactor

--- sbml_generator/sbml_gen_impl.py
+++ sbml_generator/sbml_gen_impl.py
@@ -13,46 +13,44 @@ class GenerateSBML(object):
         assert all(isinstance(r, tuple) for r in reactions)
         self.graph = graph
         self.reactions = reactions
+        self.doc = SBMLGenerator(reactions=self.reactions).document
+        self.model = self.doc.createModel()
+        self.seen_reagents = set()
 
     def generate_sbml(self):
-        doc = SBMLGenerator(reactions=self.reactions).document
-        model = doc.createModel()
-        SBMLGenerator.check(model, 'create model')
-
-        seen_reagents = set()
         for reaction in self.reactions:
-            current_reaction = self.add_reaction(model, reaction)
+            current_reaction = self.add_reaction(reaction)
             reactants, products = reaction
-            for reagent in reactants:
-                species_reference = current_reaction.createReactant()
-                self.add_reagents_to_reaction(reagent, species_reference)
-                self.add_species(model, reagent, seen_reagents)
-            for reagent in products:
-                species_reference = current_reaction.createProduct()
-                self.add_reagents_to_reaction(reagent, species_reference)
-                self.add_species(model, reagent, seen_reagents)
-        return doc
+            for species in reactants:
+                reaction_ref = current_reaction.createReactant()
+                self.add_species_to_model(species)
+                self.add_species_to_reaction(species, reaction_ref)
+            for species in products:
+                reaction_ref = current_reaction.createProduct()
+                self.add_species_to_model(species)
+                self.add_species_to_reaction(species, reaction_ref)
+        return self.doc
 
-    def add_reaction(self, model, reaction):
-        current_reaction = model.createReaction()
+    def add_reaction(self, reaction):
+        current_reaction = self.model.createReaction()
         reaction_name = ExpandGraph.human_readable_reaction(self.graph, reaction)
         reaction_name = self.replace_non_alphanumeric_with_underscore(reaction_name)
         SBMLGenerator.check(current_reaction.setId(reaction_name), 'set reaction id')
         # SBMLGenerator.check(r1.setReversible(False), 'set reaction reversibility flag')
         return current_reaction
 
-    def add_species(self, model, reagent, seen_reagents):
-        if reagent not in seen_reagents:
-            species = model.createSpecies()
+    def add_species_to_model(self, reagent):
+        if reagent not in self.seen_reagents:
+            species = self.model.createSpecies()
             name = ExpandGraph.node_name(self.graph, reagent)
             name = self.replace_non_alphanumeric_with_underscore(name)
             SBMLGenerator.check(species.setId(name), 'set species id')
-            seen_reagents.add(reagent)
+            self.seen_reagents.add(reagent)
 
-    def add_reagents_to_reaction(self, reagent, species_reference):
+    def add_species_to_reaction(self, reagent, reaction_reference):
         name = ExpandGraph.node_name(self.graph, reagent)
         name = GenerateSBML.replace_non_alphanumeric_with_underscore(name)
-        SBMLGenerator.check(species_reference.setSpecies(name), 'assign species')
+        SBMLGenerator.check(reaction_reference.setSpecies(name), 'assign species')
 
     @staticmethod
     def replace_non_alphanumeric_with_underscore(string):
