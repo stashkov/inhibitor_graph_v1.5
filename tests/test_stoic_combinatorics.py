53a646030ad032e6b6f0f38b5f1cfacc470caac2
Stashkov, Vladimir <vladimir.stashkov>
2017-09-09 22:56:07 +0200

    refactor

--- tests/test_stoic_combinatorics.py
+++ tests/test_stoic_combinatorics.py
@@ -3,6 +3,13 @@ from stoic.stoic_combinatorics import ExpandGraphCombinatorics
 
 
 class TestConcreteExamples(object):
+    def setup_method(self, method):
+        print("Starting execution of: {}".format(method.__name__))
+        self.graph = nx.DiGraph()
+        self.graph.add_node(1, name='U')
+        self.graph.add_node(2, name='V')
+        self.graph.add_node(3, name='N')
+
     def test_expand_2_activation_edges_complex(self):
         """
         edges
@@ -13,12 +20,8 @@ class TestConcreteExamples(object):
             U + V + not(N) <-> U:V:N -> U + V + N
             N -> not(N)
         """
-        g = nx.DiGraph()
-        g.add_node(1, name='U')
-        g.add_node(2, name='V')
-        g.add_node(3, name='N')
-        g.add_edge(1, 3, weight=0)
-        g.add_edge(2, 3, weight=0)
+        self.graph.add_edge(1, 3, weight=0)
+        self.graph.add_edge(2, 3, weight=0)
 
         matrix_gold = [
             [-1, 1, 0],  # U
@@ -28,13 +31,9 @@ class TestConcreteExamples(object):
             [1, -1, 0]  # U:V:N
         ]
         vector_gold = [1, 0, 0]
-
-        result = ExpandGraphCombinatorics(g)
-
-        matrix_lead = result.matrix
-        vector_lead = result.vector
-        assert matrix_lead == matrix_gold
-        assert vector_lead == vector_gold
+        result = ExpandGraphCombinatorics(self.graph)
+        assert result.matrix == matrix_gold
+        assert result.vector == vector_gold
 
     def test_expand_mixed_reaction(self):
         """
@@ -46,28 +45,18 @@ class TestConcreteExamples(object):
             N -> not(N)
             V + N <-> V:N -> V + N
         """
-        g = nx.DiGraph()
-        g.add_node(1, name='U')
-        g.add_node(2, name='V')
-        g.add_node(3, name='N')
-        g.add_edge(1, 3, weight=1)
-        g.add_edge(2, 3, weight=0)
+        self.graph.add_edge(1, 3, weight=1)
+        self.graph.add_edge(2, 3, weight=0)
 
         matrix_gold = [
             [-1, 1, 0, 0, 0],  # U
-            [0, 0, 0, - 1, 1],  # V
-            [-1, 0, - 1, - 1, 1],  # N
+            [0, 0, 0, -1, 1],  # V
+            [-1, 0, -1, -1, 1],  # N
             [0, 1, 1, 0, 0],  # not(N)
-            [1, - 1, 0, 0, 0],  # U:N
-            [0, 0, 0, 1, - 1]  # V:N
+            [1, -1, 0, 0, 0],  # U:N
+            [0, 0, 0, 1, -1]  # V:N
         ]
         vector_gold = [1, 0, 0, 1, 0]
-
-        result = ExpandGraphCombinatorics(g)
-
-        print(result.matrix)
-        print(matrix_gold)
-        matrix_lead = result.matrix
-        vector_lead = result.vector
-        assert matrix_lead == matrix_gold
-        assert vector_lead == vector_gold
+        result = ExpandGraphCombinatorics(self.graph)
+        assert result.matrix == matrix_gold
+        assert result.vector == vector_gold
